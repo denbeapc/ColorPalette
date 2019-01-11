2823a5dff2d1fcc589d9fadf7d5d9adbdd4716f4
Pierson Denbeau <denbeapc@gmail.com>
2019-01-11 16:24:18 -0500

    Init project

---
+++ ColorPalette/ColorPalette/Views/AboutPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ColorPalette.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
